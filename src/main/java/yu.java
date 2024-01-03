7cf2db9f59613b05b94f0bd3e655dc25436986dd
yuyu <153343507@qq.com>
2024-01-03 13:26:13 +0800

    记录提交日志信息

---
+++ src/main/java/yu.java
@@ -0,0 +1,7 @@
+public class yu {
+    public static void main(String[] args){
+        System.out.println("weitianyu 22550201049");
+        System.out.println("weitianyu 22550201049");
+        System.out.println("weitianyu 22550201049");
+    }
+}
